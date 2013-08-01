04acf82d63751cca69567e5184093b71a2b7cb6a
ial <air_pc@mail.ru>
2013-08-02 01:25:28 +0400

    initial commit

---
+++ async_executor/async_executor.cpp
@@ -0,0 +1,30 @@
+//
+//  async_executor.cpp
+//  Thread1
+//
+//  Created by Igor on 01.08.13.
+//  Copyright (c) 2013 Igor. All rights reserved.
+//
+
+#include "async_executor.h"
+
+namespace Concrete
+{
+    
+    class AsyncExecutor
+    {
+      //void
+    };
+    
+    class AsyncExecutor
+    {
+    public:
+        AsyncExecutor()
+        {
+        }
+        
+        
+    };
+    
+    //ExecuteAsync
+}
---
+++ async_executor/async_executor.h
@@ -0,0 +1,14 @@
+//
+//  async_executor.h
+//  Thread1
+//
+//  Created by Igor on 01.08.13.
+//  Copyright (c) 2013 Igor. All rights reserved.
+//
+
+#ifndef __Thread1__async_executor__
+#define __Thread1__async_executor__
+
+#include <iostream>
+
+#endif /* defined(__Thread1__async_executor__) */
